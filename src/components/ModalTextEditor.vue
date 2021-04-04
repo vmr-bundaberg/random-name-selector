1c60366480ea750013889efce02ed6e5e1f12bc5
pontakornth <pontakorn61@gmail.com>
2021-04-04 22:09:39 +0700

    feat: Make modal working

--- src/components/ModalTextEditor.vue
+++ src/components/ModalTextEditor.vue
@@ -1,23 +1,32 @@
 <template>
-    <button class="button">
+    <button @click="toggle" class="button">
         Edit names
     </button>
     <teleport to="body">
-        <div class="absolute top-0 left-0 w-full h-full bg-gray-800 bg-opacity-40 flex">
+        <div :class="`modal ${isOpen && 'modal-open'}`">
             <div class="m-auto w-3/4 min-h-1/4 flex flex-col items-center p-4 bg-white">
                <label class="text-left self-start my-2">Insert names here (seperate by newline)</label>
                <textarea rows="10" class="border p-2 w-full" />
-               <button class="button button-green my-4">Save</button>
+               <button @click="toggle" class="button button-green my-4">Save</button>
             </div>
         </div>
     </teleport>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
-    setup() {},
+    setup() {
+        const isOpen = ref(false)
+        const toggle = () => {
+            isOpen.value = !isOpen.value
+        }
+        return {
+            isOpen,
+            toggle
+        }
+    },
 })
 </script>
 
@@ -27,6 +36,12 @@ export default defineComponent({
     @apply px-4 py-2 font-semibold rounded bg-indigo-500 hover:bg-indigo-600 text-white;
     &-green {
         @apply bg-green-500 hover:bg-green-600;
+    }
+}
+.modal {
+    @apply absolute top-0 left-0 w-full h-full bg-gray-800 bg-opacity-40 hidden;
+    &-open {
+        @apply flex;
     }
 }
 </style>
