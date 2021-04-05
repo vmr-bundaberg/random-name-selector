3604c45c2195167523c23a4c153126763aed4d1c
pontakornth <pontakorn61@gmail.com>
2021-04-05 14:06:01 +0700

    feat: Add randomizing function

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
         <modal-text-editor v-model="names" />
       </div>
       <div class="py-4">
-        <h2 class="text-4xl font-bold mb-4">Christopher Meow</h2>
-        <Button>
-          Random name
+        <h2 class="text-4xl font-bold mb-4">{{ selectedName }}</h2>
+        <Button @click="randomName" :disabled="names.length === 0">
+          Randomize Name
         </Button>
       </div>
   </div>
@@ -17,6 +17,13 @@
 import { computed, defineComponent, ref } from 'vue'
 import ModalTextEditor from './components/ModalTextEditor.vue'
 import Button from './components/Button.vue'
+// Credit to Laurens Holst (https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
+function shuffleArray(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 export default defineComponent({
   name: 'App',
@@ -27,9 +34,24 @@ export default defineComponent({
   setup() {
     const names = ref("")
     const nameList = computed(() => names.value.split('\n'))
+    const selectedName = ref("Jonathan Joestar")
+    const randomName = () => {
+      const copiedNameList = [...nameList.value]
+      shuffleArray(copiedNameList)
+      const timeToStop = Math.random() * (3000 - 100) + 1000
+      let index =  0
+      for (let i = 0; i < timeToStop; i += 100) {
+        setTimeout(() => {
+          selectedName.value = copiedNameList[(i /100) % copiedNameList.length]
+        }, i)
+      }
+    }
+
     return {
       names,
-      nameList
+      nameList,
+      selectedName,
+      randomName,
     }
   }
 })
@@ -47,9 +69,5 @@ html, body {
   color: #2c3e50;
   height: 100%;
   display: flex;
-}
-
-function ref(arg0: string) {
-  throw new Error('Function not implemented.')
 }
 </style>
